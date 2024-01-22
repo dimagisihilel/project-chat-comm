686934f742d7d2fdc8b8e40a2a02c51b921ddcf1
dimagisihilel <gunarathneedirimuni@gmail.com>
2024-01-22 12:29:40 +0530

    modify the Launcher

--- src/main/java/lk/ijse/chatcomm/Launcher.java
+++ src/main/java/lk/ijse/chatcomm/Launcher.java
@@ -1,4 +1,24 @@
 package lk.ijse.chatcomm;
 
-public class Launcher {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Launcher extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(this.getClass().getResource("/view/login_form.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        //stage.setResizable(false);
+        //stage.centerOnScreen();
+        stage.setTitle("Chat Comm");
+        stage.show();
+    }
 }
--- src/main/java/lk/ijse/chatcomm/controller/LoginFormController.java
+++ src/main/java/lk/ijse/chatcomm/controller/LoginFormController.java
@@ -2,10 +2,16 @@ package lk.ijse.chatcomm.controller;
 
 import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
 
 public class LoginFormController {
 
     public Button btnLogin;
+    public TextField txtUserName;
+    public AnchorPane root;
+
 
     public void btnLoginOnAction(ActionEvent actionEvent) {
 
